11397a6cc51bcfe705e74c6216c9d23c2881d6ea
Hippolyte <hippolyte.aubert@epitech.eu>
2022-01-29 16:56:11 +0100

    Scene for Main Menu, Pause Menu and Losing Screen

---
+++ New Unity Project/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public Text theText;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = Color.red;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = Color.white;
+    }
+}
